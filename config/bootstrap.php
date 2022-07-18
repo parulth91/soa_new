4d430221c6d620881b1cd334f9ff78f3a0e51c06
parulth91 <parul.th91@gmail.com>
2022-07-18 06:36:27 -0700

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -219,14 +219,14 @@ Type::build('timestamp')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
-//if (Configure::read('debug')) {
-  // Plugin::load('DebugKit', ['bootstrap' => true]);
-//}
+if (Configure::read('debug')) {
+   Plugin::load('debugKit', ['bootstrap' => true]);
+}
  Plugin::loadAll();
 Plugin::load('Bootstrap', ['bootstrap' => true]);
 
 
-Plugin::load('wyriHaximus/twigView', ['bootstrap' => true]);
+Plugin::load('wyrihaximus/twigView', ['bootstrap' => true]);
 
 Configure::write('Users.config', ['users']);
 Plugin::load('CakeDC/Users', ['routes' => true, 'bootstrap' => true]);
