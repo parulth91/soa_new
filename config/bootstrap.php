e6eac46fd33e92dd60e3133aba851fc9fa978a9b
parulth91 <parul.th91@gmail.com>
2022-07-18 23:15:07 -0700

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -226,7 +226,7 @@ Type::build('timestamp')
 Plugin::load('Bootstrap', ['bootstrap' => true]);
 
 
-Plugin::load('wyrihaximus/twig-view', ['bootstrap' => true]);
+Plugin::load('vendor/*', ['bootstrap' => true]);
 
 Configure::write('Users.config', ['users']);
 Plugin::load('CakeDC/Users', ['routes' => true, 'bootstrap' => true]);
